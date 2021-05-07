e44b8de18681f004ce8a4243eb0838f50adc2169
Aleksa <aleksa.lukic.3659@metropolitan.ac.rs>
2021-05-07 13:45:43 +0200

    Generate navbar component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,6 +12,7 @@ import {HttpClientModule} from '@angular/common/http';
 import {AdminComponent} from './components/admin/admin.component';
 import {DashboardComponent} from './components/admin/dashboard/dashboard.component';
 import {HashLocationStrategy, LocationStrategy} from '@angular/common';
+import { NavbarComponent } from './components/navbar/navbar.component';
 
 @NgModule({
   declarations: [
@@ -19,7 +20,8 @@ import {HashLocationStrategy, LocationStrategy} from '@angular/common';
     LoginComponent,
     FormInputComponent,
     AdminComponent,
-    DashboardComponent
+    DashboardComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
