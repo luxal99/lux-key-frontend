4ed2d7f0122786807c4e9503c7a0f12101ffbb93
Aleksa <aleksa.lukic.3659@metropolitan.ac.rs>
2021-05-15 11:21:09 +0200

    Remo .spec files

--- src/app/pipes/car-brand.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { CarBrandPipe } from './car-brand.pipe';
-
-describe('CarBrandPipe', () => {
-  it('create an instance', () => {
-    const pipe = new CarBrandPipe();
-    expect(pipe).toBeTruthy();
-  });
-});
