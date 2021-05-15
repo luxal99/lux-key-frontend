0223c046b26e659f25bebd10c00d349b9c64bfb3
Aleksa <aleksa.lukic.3659@metropolitan.ac.rs>
2021-05-15 11:42:53 +0200

    Fix bugs

--- src/app/service/key.service.ts
+++ src/app/service/key.service.ts
@@ -2,10 +2,15 @@ import {Injectable} from '@angular/core';
 import {GenericService} from './generic.service';
 import {Key} from '../models/key';
 import {RestRoutes} from '../constant/const';
+import {Observable} from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class KeyService extends GenericService<Key> {
   route = RestRoutes.KEY;
+
+  findCriticalAmountKeys(): Observable<Key[]> {
+    return this.http.get<Key[]>(this.route + '/critical-amount');
+  }
 }
