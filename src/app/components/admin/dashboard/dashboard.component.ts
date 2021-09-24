0a5209f94fdf18d51a198ae7f84eec031ac6f737
Aleksa Lukic <lukicaleksa04@gmail.com>
2021-09-24 12:29:02 +0200

    Search key

--- src/app/components/admin/dashboard/dashboard.component.ts
+++ src/app/components/admin/dashboard/dashboard.component.ts
@@ -1,15 +1,52 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
+import { Key } from '../../../models/key';
 
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
-  styleUrls: ['./dashboard.component.sass']
+  styleUrls: ['./dashboard.component.sass'],
 })
 export class DashboardComponent implements OnInit {
 
-  constructor() { }
+
+  listOfKey: Key[] = [];
+  searchText = '';
+
+  constructor() {
+  }
+
+  searchForm = new FormGroup({
+    search: new FormControl({}),
+  });
 
   ngOnInit(): void {
   }
 
+  inputChange(): void {
+    if (this.searchText.length === 0) {
+      // @ts-ignore
+      document.getElementById('search').style.display = 'none';
+      // @ts-ignore
+      document.getElementById('huge-label').style.display = 'block';
+
+      // @ts-ignore
+      document.getElementById('centered').style.minHeight = '70vh';
+      this.listOfKey = [];
+    } else if (this.searchText.length > 1) {
+      // @ts-ignore
+      document.getElementById('search').style.display = 'block';
+      // @ts-ignore
+      document.getElementById('huge-label').style.display = 'none';
+      // @ts-ignore
+      document.getElementById('centered').style.minHeight = '30vh';
+    }
+  }
+
+  showSearch(): void {
+    // @ts-ignore
+    document.getElementById('search').style.display = 'block';
+    // @ts-ignore
+    document.getElementById('huge-label').style.display = 'none';
+  }
 }
