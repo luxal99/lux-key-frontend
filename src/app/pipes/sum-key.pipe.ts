c555cea1ef76c6a41f602ee5a470fb6dfaba705a
Aleksa Lukic <lukicaleksa04@gmail.com>
2021-10-20 09:41:08 +0200

    Fix bugs

---
+++ src/app/pipes/sum-key.pipe.ts
@@ -0,0 +1,16 @@
+import { Pipe, PipeTransform } from '@angular/core';
+import { Key } from '../models/key';
+
+@Pipe({
+  name: 'sumKey',
+})
+export class SumKeyPipe implements PipeTransform {
+
+  transform(listOfKey: Key[]): number {
+    console.log(listOfKey);
+    let total = 0;
+    listOfKey.filter((item) => total += item.idCurrentPrice.price);
+    return total;
+  }
+
+}
