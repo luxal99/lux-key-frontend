8f5d67b5461f63157356c880a4bd7b2da8d37925
Aleksa Lukic <lukicaleksa04@gmail.com>
2021-12-02 08:51:59 +0100

    Built in reports

--- src/app/service/report.service.ts
+++ src/app/service/report.service.ts
@@ -2,7 +2,9 @@ import { Injectable } from '@angular/core';
 import { GenericService } from './generic.service';
 import { Report } from '../models/Report';
 import { RestRoutes } from '../constant/const';
+import { Observable } from 'rxjs';
 
+type ReportType = 'BUILT_IN' | 'IN_STOCK';
 @Injectable({
   providedIn: 'root',
 })
