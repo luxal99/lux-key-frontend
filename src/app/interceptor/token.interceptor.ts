9b00e834192078362bd180850d402ccbea428485
Aleksa Lukic <lukicaleksa04@gmail.com>
2021-12-19 12:41:47 +0100

    Fix bugs

--- src/app/interceptor/token.interceptor.ts
+++ src/app/interceptor/token.interceptor.ts
@@ -5,7 +5,7 @@ import { TOKEN_NAME } from "../constant/const";
 
 export class TokenInterceptor implements HttpInterceptor {
   intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
-    const secureToken = sessionStorage.getItem(TOKEN_NAME);
+    const secureToken = localStorage.getItem(TOKEN_NAME);
     const modifiedReq = req.clone({
       headers: req.headers.set(TOKEN_NAME, `${secureToken}`),
     });
